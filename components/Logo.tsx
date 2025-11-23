7e61e65db40593f7da4a6aca4231e77bc6a61b2f
yduanrech <yduanrecheffting@gmail.com>
2025-11-22 22:52:09 -0300

    Updates

--- components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LogoSvg from '../images/Logo-Letra-Preta-Sem-Fundo.svg';
-
-export const Logo: React.FC<{ className?: string }> = ({ className = "w-10 h-10" }) => {
-  return (
-    <img src={LogoSvg} alt="Centro de Cópias e Plotagem" className={className} />
-  );
-};
